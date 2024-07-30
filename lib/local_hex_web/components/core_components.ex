18ee6ec60b648850fa58ad8146060f8f13ee6d68
Ingo Müller <ingo.mue@pm.me>
2024-07-30 17:25:15 +0200

    No support for elixir 1.12.3 anymore

--- lib/local_hex_web/components/core_components.ex
+++ lib/local_hex_web/components/core_components.ex
@@ -16,6 +16,7 @@ defmodule LocalHexWeb.CoreComponents do
   """
   use Phoenix.Component
 
+  alias Phoenix.HTML.Form
   alias Phoenix.LiveView.JS
 
   @doc """
@@ -301,7 +302,7 @@ defmodule LocalHexWeb.CoreComponents do
   def input(%{type: "checkbox"} = assigns) do
     assigns =
       assign_new(assigns, :checked, fn ->
-        Phoenix.HTML.Form.normalize_value("checkbox", assigns[:value])
+        Form.normalize_value("checkbox", assigns[:value])
       end)
 
     ~H"""
